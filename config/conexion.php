a070c9d275dbb7076536716d2588893f450fe941
cardonav <cardonawilder404@gmail.com>
2026-01-14 16:35:07 -0500

    cambio3

--- config/conexion.php
+++ config/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'tienda_tech';
+$host = 'sql207.infinityfree.com';
+$user = 'if0_40817454';
+$pass = 'Iy59XK87XpG9Id';
+$db = 'if0_40817454_tienda_tech1';
 
 $conexion = new mysqli($host, $user, $pass, $db);
 
